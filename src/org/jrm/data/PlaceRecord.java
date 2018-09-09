63d9e300fd8bfe07c4b9ff8fcd666cb513d114f3
Jared R. Mallas <jrtytn@gmail.com>
2018-09-09 17:18:35 -0500

    base data instance classes

---
+++ src/org/jrm/data/PlaceRecord.java
@@ -0,0 +1,10 @@
+package org.jrm.data;
+
+/**
+ * Class model for a place record from input file
+ * @author Jared Mallas
+ * @version 1.0
+ */
+public class PlaceRecord
+{
+}
---
+++ src/org/jrm/data/StuffRecord.java
@@ -0,0 +1,9 @@
+package org.jrm.data;
+
+/**
+ * Class model for stuff record
+ * @author Jared Mallas
+ * @version 1.0
+ */
+public class StuffRecord {
+}
